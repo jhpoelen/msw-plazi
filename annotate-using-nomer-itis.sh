91a6addcf5a758eaf14df2f0f9fed48674e6b972
Jorrit Poelen <jhpoelen+git@jhpoelen.nl>
2023-01-18 12:55:27 -0600

    add some not included in ITIS claims to annotations.csv; if you can, please review added statement @jar398 @n8upham in context of https://github.com/globalbioticinteractions/nomer/issues/114

--- annotate-using-nomer-itis.sh
+++ annotate-using-nomer-itis.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
 #
-# takes first column from period separated file (like msw2-verbatim)
+# takes first column from period separated file (like msw2.json)
 # parses the value
 # then report non-matches against ITIS
 #
-grep -oP "^[^.]+" | sed 's/^/ \t/g' | nomer replace gbif-parse | nomer append itis | grep NONE | grep -v Family | grep -v Subfamily | grep -v Order | grep -v Suborder | cut -f2 | sed 's+$+,!,ITIS,"José Augusto Salim, & Jorrit Poelen. (2022). globalbioticinteractions/nomer: 0.4.7 (0.4.7). Zenodo. https://doi.org/10.5281/zenodo.7411758",2023-01-18,,,,,,,+g'
+# example usage:
+#  cat msw2.json | ./annotate-using-nomer-itis.sh
+
+jq --raw-output '[.["docId"], .["verbatimText"]] | @tsv'\
+ | grep -oP "^[^.]+"\
+ | nomer append gbif-parse\
+ | cut -f1,5\
+ | nomer append itis\
+ | grep NONE\
+ | grep -v Family\
+ | grep -v Subfamily\
+ | grep -v Order\
+ | grep -v Suborder\
+ | cut -f1,2\
+ | awk -v FS='\t' '{ print($2 ",!,ITIS,\"Salim JA, Poelen JH (2022). globalbioticinteractions/nomer: 0.4.7 (0.4.7). Zenodo. https://doi.org/10.5281/zenodo.7411758\",2023-01-18,https://tb.plazi.org/GgServer/html/" $1 ",,,,,,") }'
